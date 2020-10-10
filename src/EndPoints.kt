ce3c5bce25dcc47441bef24dbc8656bc1a568cd2
Andreev <soandreev@gumtree.com>
2020-10-10 10:42:58 +0100

    Adding comment delete endpoints. Adding get comment by person id endpoint

--- src/EndPoints.kt
+++ src/EndPoints.kt
@@ -8,4 +8,5 @@ const val TUTORIAL_ENDPOINT = "/tutorial/{$PARAM_ID}"
 const val TUTORIAL_ALL_ENDPOINT = "/tutorial"
 
 const val COMMENT_ENDPOINT = "/comment/{$PARAM_ID}"
+const val COMMENT_PERSON_ENDPOINT = "/comment/person/{$PARAM_ID}"
 const val COMMENT_ALL_ENDPOINT = "/comment"
